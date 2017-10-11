a3dbbc83161d01e0b8de6883e3cc4ac4f9ae24c7
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/extractalignedfastq/ExtractAlignedFastqTest.scala
+++ src/test/scala/nl/biopet/tools/extractalignedfastq/ExtractAlignedFastqTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.extractalignedfastq
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object ExtractAlignedFastqTest extends BiopetTest {
+class ExtractAlignedFastqTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
