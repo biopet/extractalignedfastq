cd2d8c5e0fc0df863733caf9b42db964df88cd9a
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-12-28 10:01:57 +0100

    create headers

--- src/main/scala/nl/biopet/tools/extractalignedfastq/ExtractAlignedFastq.scala
+++ src/main/scala/nl/biopet/tools/extractalignedfastq/ExtractAlignedFastq.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Sequencing Analysis Support Core - Leiden University Medical Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.extractalignedfastq
 
 import java.io.File
@@ -17,10 +38,10 @@ object ExtractAlignedFastq extends ToolCommand[Args] {
     val cmdArgs = cmdArrayToArgs(args)
 
     // Require input files to exist
-    require(cmdArgs.inputBam.exists(),"Input BAM file not found")
-    require(cmdArgs.inputFastq1.exists(),"Input FASTQ file 1 not found")
-    if (cmdArgs.inputFastq2.isDefined){
-      require(cmdArgs.inputFastq2.get.exists(),"Input FASTQ file 2 not found")
+    require(cmdArgs.inputBam.exists(), "Input BAM file not found")
+    require(cmdArgs.inputFastq1.exists(), "Input FASTQ file 1 not found")
+    if (cmdArgs.inputFastq2.isDefined) {
+      require(cmdArgs.inputFastq2.get.exists(), "Input FASTQ file 2 not found")
     }
 
     logger.info("Start")
